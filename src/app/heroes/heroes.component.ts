38050a381396139d03e5358fc370817e761412fd
mcn13 <marinacambeiro98@gmail.com>
2021-05-26 17:00:14 +0200

    empezando a crear el hero detail

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+
+
+  selectedHero: Hero | undefined;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
